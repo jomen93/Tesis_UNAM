c82d3da078551f8535b9ff5ebeb5b1e468672d93
Johan <jsmendezh@unal.edu.co>
2023-01-13 19:08:42 -0500

    add file to compresibility limit

---
+++ limite_compresibilidad.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Author: Johan Méndez
+Created on: 2023-01-13
+Description: File to calculate compresibility limit in the binary start systems 
+"""
+
+import numpy as np 
